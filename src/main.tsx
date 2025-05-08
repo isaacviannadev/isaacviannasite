4f582e334766f88d9bd1d5434dcd6dcf834c95bd
Isaac <isaac.vianna@twistag.com>
2025-05-08 10:41:06 +0100

    feat: change import from GlobalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GlobalStyles from "./global";
-import "./i18n";
 import { LanguageProvider } from "./contexts/LanguageContext";
+import { GlobalStyles } from "./global";
+import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
